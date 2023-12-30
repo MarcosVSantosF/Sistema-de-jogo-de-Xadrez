7b0da94b6731cb58e827fa5292b37090609fb7f6
Marcos Vinicius <marcosvsantosf@gmail.com>
2023-12-30 11:34:48 -0300

    implementando as restricoes de movimento

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Seção_12___Projeto___Sistema_de_jogo_de_xadrez;
+using tabuleiro;
+using xadrez;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+
+                while(!partida.terminada)
+                {
+                    try
+                    {
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        System.Console.WriteLine();
+                        System.Console.WriteLine("Turno: " + partida.turno);
+                        System.Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
+                        System.Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
+
+
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                        System.Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem , destino);
+                        
+
+                        partida.realizaJogada(origem, destino);
+                    }
+                    catch (TabuleiroException e)
+                    {
+                        System.Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (TabuleiroException e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ tabuleiro/TabuleiroException.cs
@@ -0,0 +1,8 @@
+﻿namespace Seção_12___Projeto___Sistema_de_jogo_de_xadrez;
+
+public class TabuleiroException : Exception
+{
+    public TabuleiroException(string msg) : base(msg)
+    {
+    }
+}
